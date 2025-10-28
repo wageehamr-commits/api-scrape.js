d5e2f236d4f596cfde2691cbe8d11119a43c4596
wageehamr-commits <wageeh.amr@gmail.com>
2025-10-28 15:46:32 +0300

    Create scrape.js

---
+++ api/scrape.js
@@ -0,0 +1,26 @@
+import puppeteer from "puppeteer";
+
+export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) {
+    res.status(400).send("Missing URL");
+    return;
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
+    const html = await page.content();
+    await browser.close();
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.status(200).send(html);
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
+}
